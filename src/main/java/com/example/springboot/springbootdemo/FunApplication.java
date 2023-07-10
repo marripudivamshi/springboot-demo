86606c36ab78da310a25f3d01ba893cae419bbe0
Marripudi, Vamshi Krishna <krishna.m.dev023@gmail.com>
2023-07-09 22:11:36 -0500

    adding springboot demo project

---
+++ src/main/java/com/example/springboot/springbootdemo/FunApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboot.springbootdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunApplication {
+	
+	@GetMapping("/")
+	public String WelcomeController() {
+		return "Hello World";
+		
+	}
+}
